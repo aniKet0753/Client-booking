b90c93cfc6c65518d8fff242a24a3cf301e762e6
Abhishek Kumar Mahto <abhishekkumarmahto2005@gmail.com>
2025-06-04 02:53:02 +0530

    api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: "https://backend-reboot-l2g.onrender.com/",
-  baseURL: "http://localhost:5001/",
+  // baseURL: "http://localhost:5001/",
   headers: {
     'Content-Type': 'multipart/form-data'
   }
